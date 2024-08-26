05127ddffcbcd1d84619ebcecb56df31cdd43c7f
Raiela Quirnio <raielaquirino@gmail.com>
2024-08-26 20:01:33 -0300

    First Question

---
+++ L1-D1DAD1/out/production/L1-D1DAD1/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+D
+src.kotlin.basico.kt,8\b\8b2c8200567cfb7907c4ee675a39bdcc5d8819f4
+G
+src/kotlin/basico/q1.kt,1\e\1e704a0d0056a70b341812ee59f301d3812c2602
+9
+	src/q1.kt,a\1\a1b89796f152af6101e7eadc5eb159f98f205c7b
---
+++ L1-D1DAD1/src/q1.kt
@@ -0,0 +1,32 @@
+package src
+
+fun main(){
+    val listaCursos: MutableList<String> = mutableListOf(
+        "Sistemas para Dispositivos Móveis",
+        "Análise e Desevolvimento de Sistemas")
+    listaCursos.add("Técnico em Informática para Internet")
+    listaCursos.add("Manutenção de Aeronaves")
+    listaCursos.add("Técnico em Células")
+    listaCursos.add("Processos Gerenciais")
+
+    val filterList = getNewList(listaCursos, "Sistemas")
+    printList(filterList)
+}
+
+fun printList(listNew: MutableList<String>){
+    listNew.forEachIndexed{ index, item ->
+        println("$index - $item")
+    }
+}
+
+fun getNewList(items: MutableList<String>, findWord: String): MutableList<String> {
+    val newList: MutableList<String> = mutableListOf()
+
+    for(item in items){
+        if (item.contains(findWord, ignoreCase = true)) {
+            newList.add(item)
+        }
+    }
+
+    return newList
+}
